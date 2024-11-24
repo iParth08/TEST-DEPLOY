1b7b7186888bf78ba22cf583c220a9e293e88df5
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-11-24 22:59:10 +0530

    deploy await

--- test.js
+++ test.js
@@ -9,3 +9,13 @@ console.log("This is a safe line.");
 // This is a secret
 
 console.log("I will alter this later.");
+
+console.log(" This is from deploy branch");
+
+async function main() {
+  await axios.get("http://localhost:5000/test", {
+    headers: {
+      Authorization: `Bearer ${process.env.SECRET_KEY}`,
+    },
+  });
+}
