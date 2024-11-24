5602a3b9d69b88737b1c63c8e3bd799cc6384e0b
Shwet Prakash <krish.shwetprakash08@gmail.com>
2024-11-24 22:55:02 +0530

    base

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log("This is a safe line.");
+console.log("This is a safe line.");
+console.log("This is a safe line.");
+
+console.log(process.env.SECRET_KEY);
+
+console.log("This is a safe line.");
+
+// This is a secret
+
+console.log("I will alter this later.");
